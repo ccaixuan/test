48a0799a24ab8c98200420e8bc07c4f505680839
ccaixuan <130960385+ccaixuan@users.noreply.github.com>
2023-06-05 13:49:57 +0800

    Add files via upload

--- pen-/dist/script.js
+++ pen-/dist/script.js
@@ -1,1 +1,10 @@
 document.write("HELLO我是陳采暄!!!好喜歡(G)I-DLE ")
+
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
